d19283f1f5bd8632ee2103730b5cea73d2e357ee
Isaias <gregori1800@gmail.com>
2022-10-23 13:43:56 -0300

    login

---
+++ BackEnd/src/main/java/com/portafolio/mgb/Security/Enums/RolNombre.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portafolio.mgb.Security.Enums;
+
+
+public enum RolNombre {
+   ROLE_ADMIN, ROLE_USER 
+}
