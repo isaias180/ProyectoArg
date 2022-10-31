6fc2fdef67743a7c450146df030a9be3847ea23b
Isaias <gregori1800@gmail.com>
2022-10-31 11:46:31 -0300

    modificado

--- FrontEnd/src/app/components/experiencia/experiencia.component.ts
+++ FrontEnd/src/app/components/experiencia/experiencia.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
-
-  constructor() { }
+constructor() {
+   
+   }
 
   ngOnInit(): void {
   }
