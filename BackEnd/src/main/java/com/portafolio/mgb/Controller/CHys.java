f8a8bd2fb1f9af41f79e253859e06b0259208083
Isaias <gregori1800@gmail.com>
2022-11-15 19:31:43 -0300

    modificado

--- BackEnd/src/main/java/com/portafolio/mgb/Controller/CHys.java
+++ BackEnd/src/main/java/com/portafolio/mgb/Controller/CHys.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hys")
+@RequestMapping("/skill")
 @CrossOrigin(origins = {"http://localhost:4200", "https://frondproyecto.web.app"})
 public class CHys {
 
--- FrontEnd/src/app/service/persona.service.ts
+++ FrontEnd/src/app/service/persona.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { persona } from '../model/persona.model';
 
 
@@ -9,7 +10,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'https://backendmgbheroku.herokuapp.com/personas/';
+  URL = environment.URL +'personas/';
 
   constructor(private http: HttpClient) { }
 
